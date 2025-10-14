f4db08731d8bfa11fe7c81a4d5d24ca694a433d9
myaiplug <219323215+myaiplug@users.noreply.github.com>
2025-10-14 00:10:48 -0400

    Replace MiniStudio with NoDAW Demo

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <Features />
-      <MiniStudio />
+      <NoDAW Demo />
       <Testimonials />
       <Pricing />
       <CTA />
